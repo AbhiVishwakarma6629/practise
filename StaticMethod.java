21aecdb6ccf2acc95a1bd3161b5d23ba2e771102
Anonymous <abhireso01@gmail.com>
2022-02-20 21:47:07 +0530

    implemented static method

--- StaticMethod.java
+++ StaticMethod.java
@@ -1,4 +1,13 @@
 public class StaticMethod {
-    
-    
+    public static void main(String[] args) {
+        System.out.println("Inside main");
+        StaticMethod.method1();
+    }    
+    static void method1() {
+        System.out.println("Inside method1");
+    }
+    static {
+        System.out.println("Inside the static block");
+        StaticMethod.method1();
+    }
 }
